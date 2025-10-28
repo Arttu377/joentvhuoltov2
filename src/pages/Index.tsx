129fa84f185025971f5768c6b314b404a9b1cd9d
Arttu377 <arttu.simanainen@gmail.com>
2025-10-28 15:41:56 +0200

    Fix

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -107,7 +107,7 @@ const Index = () => {
                   <svg className="w-5 h-5 text-convrt-dark-blue mr-3" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                   </svg>
-                  <a href="tel:0500724515" className="text-gray-600 hover:text-convrt-purple transition-colors">0500 724 515</a>
+                  <a href="tel:0500724515" className="text-gray-600 hover:text-[#2B4565] transition-colors">0500 724 515</a>
                 </div>
                 
                 <div className="flex items-center">
