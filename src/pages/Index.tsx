e75be88f8acd7cdd9396a8a4eb9c03d7fe869aba
Arttu377 <arttu.simanainen@gmail.com>
2025-10-28 18:15:05 +0200

    Fix

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -115,7 +115,7 @@ const Index = () => {
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                   </svg>
-                  <a href="mailto:jari.simanainen@joenlaitehuolto.fi" className="text-gray-600 hover:text-convrt-purple transition-colors">jari.simanainen@joenlaitehuolto.fi</a>
+                  <a href="mailto:jari.simanainen@joenlaitehuolto.fi" className="text-gray-600 hover:text-[#2B4565] transition-colors">jari.simanainen@joenlaitehuolto.fi</a>
                 </div>
               </div>
             </div>
